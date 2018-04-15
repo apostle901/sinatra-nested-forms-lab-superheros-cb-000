d36c1196a082fa7a391a64aa3e6d9158c02e3bb2
Jawann <jawann@gmail.com>
2018-04-15 11:10:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,15 +20,3 @@ class App < Sinatra::Base
       erb :super_hero
     end
 end
-
-post '/student' do
-    @student = Student.new(params[:student])
-
-    params[:student][:courses].each do |details|
-      Course.new(details)
-    end
-
-    @courses = Course.all
-
-    erb :student
-  end
